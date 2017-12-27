67fd2e02e6225f3b801ae25cbe97618a5b686b85
Asher Scott <asher.scott@aol.com>
2017-12-27 04:28:22 +0000

    code formatting

--- renew_ssl.py
+++ renew_ssl.py
@@ -7,6 +7,7 @@ import subprocess
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+
 CONFIG = {
     "SMTP_SERVER": "",
     "SMTP_USERNAME": "",
@@ -18,6 +19,7 @@ CONFIG = {
     "DOMAIN_NAME": ""
 }
 
+
 def certificate_expired():
     date_parts = subprocess.check_output("openssl x509 -enddate -noout -in ~/letsencrypt/{}/cert.pem".format(CONFIG['DOMAIN_NAME']), shell=True).strip("notAfter=").split(" ") 
     date_string = date_parts[1]+date_parts[0]+date_parts[3]
@@ -26,6 +28,7 @@ def certificate_expired():
     if(datetime.datetime.now() + datetime.timedelta(days=3) > expire_date):
         return True
     return False
+
 
 def generate_new_certificate():
     os.system("~/dehydrated/dehydrated --cron --config ~/letsencrypt/config --domain {} --out ~/letsencrypt --challenge http-01 ".format(CONFIG['DOMAIN_NAME']))
@@ -49,6 +52,7 @@ def send_renewal_email():
     smtp.login(CONFIG['SMTP_USERNAME'],CONFIG['SMTP_PASSWORD'])
     smtp.sendmail(CONFIG['SENDER_EMAIL_ADDRESS'], CONFIG['RECIEVER_EMAIL_ADDRESS'], email.as_string())
     smtp.quit()
+
 
 if certificate_expired():
     generate_new_certificate()
